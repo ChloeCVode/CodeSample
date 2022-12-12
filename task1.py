1b22257774ff2d6e17c62865404925bb6189eaa0
ChloeCVode <120411787+ChloeCVode@users.noreply.github.com>
2022-12-12 20:35:48 +0100

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+#   Create a function that checks if a number n
+#   is divisible by two numbers x AND y
+#   All inputs are positive, non-zero numbers.
+
+def is_divisible(n,x,y):
+    a = n % x
+    b = n % y
+    if a == 0 and b == 0:
+        return True
+    else:
+        return False
