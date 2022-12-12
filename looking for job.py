876a940eae02e08974537a2636bbd3869f980f44
ChloeCVode <120411787+ChloeCVode@users.noreply.github.com>
2022-12-13 00:40:18 +0100

    I'm looking for a job. Look into this file.

---
+++ looking for job.py
@@ -0,0 +1,10 @@
+#  I'm looking for a job as python developer
+
+#  -I have knowledge about HTML,css, currently learning javascript and still enlarge my knowledge about python
+#  which is amazing language in my opinion.
+#  -I speak fluent English and Polish
+#  -Creative and eager to learn
+
+# On this github there are few of my codes, if you found them interesting or well-written contact me!
+#                              contact: chloekontaktb@gmail.com
+
